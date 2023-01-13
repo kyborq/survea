93f110ed8244202d18e12dbf9758e363250507e7
Konstantin <k.podyganov@mail.ru>
2023-01-13 23:06:34 +0300

    #2 Finally added starting project from VS

--- src/Web.Frontend/App/src/index.tsx
+++ src/Web.Frontend/App/src/index.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
 import "./style.css";
 
-const container = document.querySelector("#app");
+const container = document.querySelector("#root") as HTMLElement;
 const root = createRoot(container);
 
 root.render(<App />);
