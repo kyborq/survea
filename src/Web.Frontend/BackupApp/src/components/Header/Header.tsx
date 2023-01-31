63949c3c4030c983e67257762216c04b4f131bd3
Aleksey Ronin <45967201+RusRonin@users.noreply.github.com>
2023-01-31 16:10:03 +0300

    Git fixed front

--- src/Web.Frontend/BackupApp/src/components/Header/Header.tsx
+++ src/Web.Frontend/BackupApp/src/components/Header/Header.tsx
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
 
-import styles from "./Header.module.css";
 import { Logo } from "../Logo/Logo";
 import { Avatar } from "../Avatar/Avatar";
 import { Button } from "../Button/Button";
+
+import styles from "./Header.module.css";
 import { Modal } from "../Modal/Modal";
 import { Input } from "../Input/Input";
-import { Map, YMaps } from "react-yandex-maps";
-import { ThumbnailUploader } from "../ThumbnailUploader/ThumbnailUploader";
 
 export const Header = () => {
-  const [eventModal, setEventModal] = useState(false);
+  const [surveyModal, setSurveyModal] = useState(false);
 
   return (
     <div className={styles.Header}>
@@ -20,36 +19,27 @@ export const Header = () => {
         <div className={styles.Actions}>
           <div className={styles.Buttons}>
             <Button
-              label="События"
+              label="Опрос"
               icon="addCircle"
               primary
-              onClick={() => setEventModal(true)}
+              onClick={() => setSurveyModal(true)}
             />
-            <Button icon="notification" />
           </div>
 
-          <Avatar
-            image="https://sun3.tele2-nn.userapi.com/impg/cJeNnCErOWnDQL2siPrlfLUm55D0gRqqHEhLmQ/THOmgW2zxH0.jpg?size=391x273&quality=96&sign=5dfaa96dbede2bab0048e78712d16d65&type=album"
-            link="/user/kyborq"
-          />
+          <Avatar />
         </div>
       </div>
 
-      {eventModal && (
+      {!surveyModal && (
         <Modal
-          title="Новое событие"
-          onClose={() => setEventModal(false)}
-          onAccept={() => setEventModal(false)}
-          onCancel={() => setEventModal(false)}
+          onClose={() => {
+            console.log("пошел нахуй");
+            setSurveyModal(false);
+          }}
+          title="Войти в профиль"
         >
-          <ThumbnailUploader />
-          <Input icon="info" placeholder="Название события" />
-          <Input icon="info" placeholder="Описание события" />
-          <Input icon="location" placeholder="Место проведения" />
-          <div style={{ display: "flex", gap: 16 }}>
-            <Input icon="calendar" placeholder="Дата" />
-            <Input icon="clock" placeholder="Время" />
-          </div>
+          <Input icon="user" placeholder="email" />
+          <Input icon="close" placeholder="password" />
         </Modal>
       )}
     </div>
--- src/Web.Frontend/BackupApp/src/components/Logo/Logo.tsx
+++ src/Web.Frontend/BackupApp/src/components/Logo/Logo.tsx
@@ -1,37 +1,11 @@
-import React, { useState } from "react";
-import { groupWords } from "../../utils/groupWords";
-import { Input } from "../Input/Input";
-import { Modal } from "../Modal/Modal";
+import React from "react";
 
 import styles from "./Logo.module.css";
 
 export const Logo = () => {
-  const [citySelect, setCitySelect] = useState(false);
-
-  const handleSelectCity = () => setCitySelect(true);
-
-  const cities = groupWords([
-    "Йошкар-Ола",
-    "Москва",
-    "Санкт-петербург",
-    "Казань",
-    "Краснодар",
-    "Абакан",
-  ]);
-
   return (
     <div className={styles.Logo}>
-      <h1 className={styles.Title}>Афишер</h1>
-      <span className={styles.City} onClick={handleSelectCity}>
-        Москва
-      </span>
-
-      {citySelect && (
-        <Modal title="Выберите город" onClose={() => setCitySelect(false)}>
-          <Input icon="search" placeholder="Москва" />
-          {JSON.stringify(cities)}
-        </Modal>
-      )}
+      <h1 className={styles.Title}>Survea</h1>
     </div>
   );
 };
